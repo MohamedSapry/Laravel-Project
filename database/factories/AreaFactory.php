197c0b9ab82e6ac5b24fb44196d4bd38e2099da9
MohamedSapry <msapry67@gmail.com>
2022-02-03 16:52:30 +0200

    Factories Created

---
+++ database/factories/AreaFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AreaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+            'city' => $this->faker->city(),
+            'country' => $this->faker->country()
+        ];
+    }
+}
