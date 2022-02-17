98f41b1782f4baad1da2c2437716c5f4136dc244
MohamedSapry <msapry67@gmail.com>
2022-02-17 11:07:34 +0200

    include app layout instead of dublicate the code

--- resources/views/layouts/usertabel.blade.php
+++ resources/views/layouts/usertabel.blade.php
@@ -29,17 +29,7 @@
         @livewireStyles
     </head>
     <body>
-    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-        <a class="dropdown-item" href="{{ route('logout') }}"
-                onclick="event.preventDefault();
-                document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
-        </a>
-
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-        </form>
-    </div>
+        @include('layouts.app')
         <livewire:user-table-self-impl>
         @livewireScripts
     </body>
