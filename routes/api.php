fcf94333e2f6aade55cbd464ad6f9b062ee8417b
MohamedSapry <msapry67@gmail.com>
2022-02-21 16:26:39 +0200

    some changes for run and test

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AddressController;
 use App\Http\Controllers\AreaController;
+use App\Http\Controllers\UserTableController;
 use App\Http\Livewire\UserTableSelfImpl;
 use App\Http\Livewire\UserFrom;
 
@@ -23,7 +24,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('addresssssss', [AddressController::class, 'getAlladdresses'] );
+Route::get('usersdata', [UserTableController::class, 'getAllUsersData'] );
+
+Route::get('addresses', [AddressController::class, 'getAlladdresses'] );
 Route::get('addresses/{id}', [AddressController::class, 'getAddress'] );
 Route::put('addresses/{id}',  [AddressController::class, 'updateAddress']);
 Route::delete('addresses/{id}', [AddressController::class, 'deleteAddress']);
